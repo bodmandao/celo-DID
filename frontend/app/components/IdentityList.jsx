7134e6943b09f1e716cb1714b632b8540765adc4
bodmandao <154741685+bodmandao@users.noreply.github.com>
2023-12-30 11:19:16 +0000

    identity listcomponent

---
+++ frontend/app/components/IdentityList.jsx
@@ -0,0 +1,19 @@
+import { ListGroup } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledListGroup = styled(ListGroup)`
+  margin-top: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export default function IdentityList({ identities }) {
+  return (
+    <StyledListGroup>
+      {identities.map((identity, index) => (
+        <ListGroup.Item key={index}>
+          {identity.name}, Age: {identity.age}
+        </ListGroup.Item>
+      ))}
+    </StyledListGroup>
+  );
+}
