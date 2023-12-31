ff2ebda75e31b933eaa7ee9c0b57c022886a9b72
bodmandao <154741685+bodmandao@users.noreply.github.com>
2023-12-31 18:06:42 +0000

    modify contract

--- backend/contracts/Did.sol
+++ backend/contracts/Did.sol
@@ -1,138 +1,143 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/**
- * @title Decentralized Identity Smart Contract
- * @dev Implements a decentralized identity system on the CELO blockchain.
- */
 contract DecentralizedIdentity {
-    /**
-     * @dev Struct to represent identity information.
-     */
+    // Struct to represent an identity
     struct Identity {
         string name;
         uint age;
         bool exists;
-        address owner;
+        bool verified;
+        bool revoked;
     }
 
-    // Mapping to store identities with their corresponding  addresses
+    // Mapping to store identity details by address
     mapping(address => Identity) public identities;
-    // Mapping to store the order in which identities were created
+
+    // Mapping to store addresses of identities by index
     mapping(uint => address) public identitiesByIndex;
-    // Mapping to track whether an address has a valid identity
-    mapping(address => bool) public hasIdentity;
 
-    // Event emitted when a new identity is created
+    // Counter to keep track of the number of identities
+    uint public identityCount;
+
+    // Events to log identity-related actions
     event IdentityCreated(address indexed owner, string name, uint age);
-    // Event emitted when identity information is updated
     event IdentityUpdated(address indexed owner, string newName, uint newAge);
-    // Event emitted when access is revoked
-    event AccessRevoked(address indexed owner);
+    event IdentityVerified(address indexed owner);
+    event IdentityRevoked(address indexed owner);
+    event IdentityDeleted(address indexed owner);
 
     /**
-     * @dev Modifier to restrict access to the owner only.
+     * @dev Modifier to ensure that the caller is the owner of the identity
      */
-    modifier onlyOwner {
+    modifier onlyIdentityOwner() {
         require(identities[msg.sender].exists, "Identity does not exist");
-        require(identities[msg.sender].owner == msg.sender, "Not the owner");
         _;
     }
 
     /**
-     * @dev Modifier to check if the caller has a valid identity.
+     * @dev Modifier to ensure that the caller is a verified identity
      */
-    modifier onlyValidIdentity {
-        require(hasIdentity[msg.sender], "Identity does not exist");
+    modifier onlyVerifier() {
+        require(identities[msg.sender].exists && identities[msg.sender].verified, "Not a verified identity");
         _;
     }
 
     /**
-     * @dev Function to create a new identity.
-     * @param _name The name of the identity owner.
-     * @param _age The age of the identity owner.
+     * @dev Modifier to ensure that the caller is a verified identity eligible to revoke another identity
+     * @param identityOwner The address of the identity to be revoked
      */
-    function createIdentity(string memory _name, uint _age) external {
+    modifier onlyRevoker(address identityOwner) {
+        require(identities[msg.sender].exists && identities[msg.sender].verified, "Not a verified identity");
+        require(identities[identityOwner].exists && !identities[identityOwner].revoked, "Identity not valid for revocation");
+        _;
+    }
+
+    /**
+     * @dev Modifier to ensure that the caller is a verified identity eligible to delete another identity
+     * @param identityOwner The address of the identity to be deleted
+     */
+    modifier onlyDeleter(address identityOwner) {
+        require(identities[msg.sender].exists && identities[msg.sender].verified, "Not a verified identity");
+        require(identities[identityOwner].exists, "Identity not valid for deletion");
+        _;
+    }
+
+    /**
+     * @dev Function to create a new identity
+     * @param name The name of the identity
+     * @param age The age of the identity
+     */
+    function createIdentity(string memory name, uint age) external {
         require(!identities[msg.sender].exists, "Identity already exists");
+        require(bytes(name).length > 0, "Name cannot be empty");
 
-        Identity storage newIdentity = identities[msg.sender];
-        newIdentity.name = _name;
-        newIdentity.age = _age;
-        newIdentity.exists = true;
-        newIdentity.owner = msg.sender;
+        identities[msg.sender] = Identity(name, age, true, false, false);
+        identitiesByIndex[identityCount + 1] = msg.sender;
+        identityCount++;
 
-        hasIdentity[msg.sender] = true;
-        identitiesByIndex[block.number - 1] = msg.sender; // Store the index for getAllIdentities
-
-        emit IdentityCreated(msg.sender, _name, _age);
+        emit IdentityCreated(msg.sender, name, age);
     }
 
     /**
-     * @dev Function to update identity information.
-     * @param _newName The new name of the identity owner.
-     * @param _newAge The new age of the identity owner.
+     * @dev Function to update an identity
+     * @param newName The new name of the identity
+     * @param newAge The new age of the identity
      */
-    function updateIdentity(string memory _newName, uint _newAge) external onlyOwner {
-        Identity storage existingIdentity = identities[msg.sender];
-        existingIdentity.name = _newName;
-        existingIdentity.age = _newAge;
+    function updateIdentity(string memory newName, uint newAge) external onlyIdentityOwner {
+        require(bytes(newName).length > 0, "Name cannot be empty");
 
-        emit IdentityUpdated(msg.sender, _newName, _newAge);
+        identities[msg.sender].name = newName;
+        identities[msg.sender].age = newAge;
+
+        emit IdentityUpdated(msg.sender, newName, newAge);
     }
 
     /**
-     * @dev Function to revoke access and delete identity.
+     * @dev Function to verify an identity
+     * @param identityOwner The address of the identity to be verified
      */
-    function revokeAccess() external onlyValidIdentity {
-        delete identities[msg.sender];
-        hasIdentity[msg.sender] = false;
+    function verifyIdentity(address identityOwner) external onlyVerifier {
+        require(!identities[identityOwner].verified, "Identity already verified");
 
-        emit AccessRevoked(msg.sender);
+        identities[identityOwner].verified = true;
+
+        emit IdentityVerified(identityOwner);
     }
 
     /**
-     * @dev Function to check if an address has a valid identity.
-     * @param _owner The address to check.
-     * @return true if the address has a valid identity, false otherwise.
+     * @dev Function to revoke an identity
+     * @param identityOwner The address of the identity to be revoked
      */
-    function hasValidIdentity(address _owner) external view returns (bool) {
-        return hasIdentity[_owner];
+    function revokeIdentity(address identityOwner) external onlyRevoker(identityOwner) {
+        require(!identities[identityOwner].revoked, "Identity already revoked");
+
+        identities[identityOwner].revoked = true;
+
+        emit IdentityRevoked(identityOwner);
     }
 
     /**
-     * @dev Function to get identity information for a given address.
-     * @param _owner The address of the identity owner.
-     * @return name The name of the identity owner.
-     * @return age The age of the identity owner.
+     * @dev Function to delete an identity
+     * @param identityOwner The address of the identity to be deleted
      */
-    function getIdentity(address _owner) external view returns (string memory, uint) {
-        Identity storage requestedIdentity = identities[_owner];
-        require(requestedIdentity.exists, "Identity does not exist");
-
-        return (requestedIdentity.name, requestedIdentity.age);
+    function deleteIdentity(address identityOwner) external onlyDeleter(identityOwner) {
+        delete identities[identityOwner];
+        emit IdentityDeleted(identityOwner);
     }
 
     /**
-     * @dev Function to get a list of all identities.
-     * @return allIdentities An array containing all valid identity addresses.
+     * @dev Function to get details of all identities
+     * @return An array of Identity structs representing all identities
      */
-    function getAllIdentities() external view returns (address[] memory) {
-    address[] memory allIdentities = new address[](block.number - 1);
-    uint index = 0;
+    function getAllIdentities() external view returns (Identity[] memory) {
+        Identity[] memory allIdentities = new Identity[](identityCount);
 
-    for (uint i = 1; i < block.number; i++) {
-        address identityOwner = identitiesByIndex[i];
-        if (identities[identityOwner].exists) {
-            allIdentities[index] = identityOwner;
-            index++;
+        for (uint i = 1; i <= identityCount; i++) {
+            address identityOwner = identitiesByIndex[i];
+            allIdentities[i - 1] = identities[identityOwner];
         }
-    }
 
-    // Resize the array to the actual number of valid identities
-    assembly {
-        mstore(allIdentities, index)
+        return allIdentities;
     }
-
-    return allIdentities;
-}
 }
